6a7ea637d4f4222059e64eef3084cd48484d74dd
Jesus <es3n1n.eng@gmail.com>
2026-06-11 01:08:16 +0300

    Fix application entry points

--- Apps/GameClient/GameClient.cpp
+++ Apps/GameClient/GameClient.cpp
@@ -5,27 +5,12 @@ int main(int argc, char* argv[])
     (void)argc;
     (void)argv;
 
-    Engine::EngineCreateInfo createInfo;
-    createInfo.ApplicationName = "GameClient";
-    createInfo.LogDirectory = "Logs/GameClient";
+    Engine::FApplicationDesc appDesc {};
+    appDesc.Title = L"Game Client";
+    appDesc.Width = 1280;
+    appDesc.Height = 720;
+    appDesc.EnableDebugRenderer = true;
+    appDesc.EnableVSync = true;
 
-    createInfo.MainWindow.Title = "Game Client";
-    createInfo.MainWindow.Width = 1280;
-    createInfo.MainWindow.Height = 720;
-    createInfo.MainWindow.StartCentered = true;
-    createInfo.MainWindow.Resizable = true;
-    createInfo.MainWindow.VisibleOnCreate = false;
-
-    createInfo.EnableRendering = true;
-    createInfo.EnableVSync = true;
-
-    createInfo.EnableFrameLimit = true;
-    createInfo.TargetFrameRate = 60;
-
-    createInfo.ClearColor.R = 0.035f;
-    createInfo.ClearColor.G = 0.045f;
-    createInfo.ClearColor.B = 0.055f;
-    createInfo.ClearColor.A = 1.0f;
-
-    return Engine::RunWindowApplication(createInfo);
+    return Engine::RunWindowApplication(appDesc);
 }
--- Apps/LevelEditor/LevelEditor.cpp
+++ Apps/LevelEditor/LevelEditor.cpp
@@ -5,27 +5,12 @@ int main(int argc, char* argv[])
     (void)argc;
     (void)argv;
 
-    Engine::EngineCreateInfo createInfo;
-    createInfo.ApplicationName = "LevelEditor";
-    createInfo.LogDirectory = "Logs/LevelEditor";
+    Engine::FApplicationDesc appDesc {};
+    appDesc.Title = L"Level Editor";
+    appDesc.Width = 1600;
+    appDesc.Height = 900;
+    appDesc.EnableDebugRenderer = true;
+    appDesc.EnableVSync = true;
 
-    createInfo.MainWindow.Title = "Level Editor";
-    createInfo.MainWindow.Width = 1600;
-    createInfo.MainWindow.Height = 900;
-    createInfo.MainWindow.StartCentered = true;
-    createInfo.MainWindow.Resizable = true;
-    createInfo.MainWindow.VisibleOnCreate = false;
-
-    createInfo.EnableRendering = true;
-    createInfo.EnableVSync = true;
-
-    createInfo.EnableFrameLimit = true;
-    createInfo.TargetFrameRate = 60;
-
-    createInfo.ClearColor.R = 0.06f;
-    createInfo.ClearColor.G = 0.065f;
-    createInfo.ClearColor.B = 0.07f;
-    createInfo.ClearColor.A = 1.0f;
-
-    return Engine::RunWindowApplication(createInfo);
+    return Engine::RunWindowApplication(appDesc);
 }
